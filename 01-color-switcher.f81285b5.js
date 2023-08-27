8f3218c9c5618b3fa65f184e5d83b5c52326a569
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2023-08-27 13:43:58 +0000

    Deploying to gh-pages from @ RafalCeglinsk/goit-js-hw-09@eb9e995545d191c1432222c4db3aa6d8ad9060ba 🚀

---
+++ 01-color-switcher.f81285b5.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d;function a(){const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=e}e.addEventListener("click",(()=>{d=setInterval(a,1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(()=>{clearInterval(d),t.disabled=!0,e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.f81285b5.js.map
