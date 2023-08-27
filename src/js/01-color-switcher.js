559b797a533545e06c0f3156367aacb55ce8ee34
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2023-08-27 15:40:04 +0200

    update of js hw 09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,5 @@
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
-intervalId = null;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
